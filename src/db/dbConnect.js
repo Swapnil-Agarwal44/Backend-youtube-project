de33c84ba4d778db302cb2f15f2f839586e85209
Swapnil Agarwal <agarwalswapnil222@gmail.com>
2025-03-01 14:24:24 +0530

    fix: improve error logging for MongoDB connection

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -10,7 +10,10 @@ const connectDB = async () => {
       `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
     ); // assignment is to console.log connectionInstance to know more about this thing.
   } catch (error) {
-    console.log("There was some error: ", error);
+    console.log(
+      "There was some error while connecting to the mongoDB database: ",
+      error
+    );
     process.exit(1); //this command is given by directly node, so there is no need to install any packages for this command. It treats every function as an exit, and it will handle any of the error through the exit code given in the command. Read the official documents for more information.
   }
 };
